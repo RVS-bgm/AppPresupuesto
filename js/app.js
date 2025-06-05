6c1379bf57826a8d21924247aad5ca6f241af9bb
RVS <m1lky.way.set@gmail.com>
2025-06-05 15:02:44 -0400

    hice el egreso y porcentaje

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 let ingresosHTML="";
 let totalIngresos=0;
 
+let EgresosHTML="";
+let totalEgresos=0;
+
+
+
 let agregarDato = (event) =>{
     event.preventDefault();
 
@@ -13,11 +18,12 @@ let agregarDato = (event) =>{
         console.log("valor :"+valor);
         if(tipo==="ingreso"){
             cargarIngresos(descripcion, Number(valor));
-            document.getElementById("descripcion").value="";
-            document.getElementById("valor").value="";
+          
         }else if(tipo ==="egreso"){
-            //hace otra cosa
+            cargarEgresos(descripcion, Number(valor)); //trabajar aca
         }
+        document.getElementById("descripcion").value="";
+        document.getElementById("valor").value="";
     }else{
         alert("Debe completar todos los campos");
     }
@@ -27,9 +33,12 @@ let agregarDato = (event) =>{
 let cargarIngresos = (descripcion, valor) =>{
     ingresosHTML += crearIngresosHTML(descripcion,valor);
     totalIngresos+=valor; //totalIngresos = TotalIngresos + valor;
+    let porcentajes = (totalEgresos/totalIngresos)*100;
+    let presupuestoTotal = (totalIngresos-totalEgresos);
     document.getElementById("IngresoTotal").textContent = formatearCLP(totalIngresos);
-    document.getElementById("presupuesto").textContent = formatearCLP(totalIngresos);//modificar
+    document.getElementById("presupuesto").textContent = formatearCLP(presupuestoTotal);//modificar
     document.getElementById("lista-ingresos").innerHTML=ingresosHTML;
+    document.getElementById("porcentaje").textContent = porcentajes+"%";
 }
 
 
@@ -54,3 +63,31 @@ function formatearCLP(numero) {
         minimumFractionDigits: 0
     }).format(numero);
 }
+
+let cargarEgresos = (descripcion, valor) =>{
+    EgresosHTML += crearEgresosHTML(descripcion,valor);
+    totalEgresos+=valor; //totaIngresos = TotalIngresos + valor;
+    let porcentajes = (totalEgresos/totalIngresos)*100;
+    let presupuestoTotal = (totalIngresos-totalEgresos);
+    document.getElementById("EgresoTotal").textContent = formatearCLP(totalEgresos);
+    document.getElementById("presupuesto").textContent = formatearCLP(presupuestoTotal);//modificar
+    document.getElementById("lista-egresos").innerHTML=EgresosHTML;
+    document.getElementById("porcentaje").textContent = porcentajes+"%";
+}
+let crearEgresosHTML=(descripcion,valor)=>{
+    return `<div class="elemento limpiarEstilos">
+                <div class="elemento_descripcion">${descripcion}</div>
+                <div class="derecha limpiarEstilos">
+                    <div class="elemento_valor">${formatearCLP(valor)}</div>
+                    <div class="elemento_eliminar">
+                        <button class="elemento_eliminar--btn">
+                            <ion-icon name="close-circle-outline"></ion-icon>
+                        </button>
+                    </div>
+                </div>
+            </div>`;
+}
+
+
+
+
